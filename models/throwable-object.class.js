b4c2f190a9e242f054f1ba7587ab91f7bc48d774
SilasSchiel <silas-schiel@gmx.de>
2024-03-03 21:42:33 +0100

    Bottle splash

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -9,9 +9,20 @@ class ThrowableObject extends MoveableObject {
         'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
     ];
 
+    IMAGES_BOTTLE_SPLASH = [
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png'
+    ];
+
     constructor(x, y) {
         super().loadImg('img/6_salsa_bottle/salsa_bottle.png');
         this.loadImages(this.IMAGES_BOTTLE);
+        this.loadImages(this.IMAGES_BOTTLE_SPLASH);
+        this.isSplash = false;
         this.x = x;
         this.y = y;
 
@@ -25,8 +36,16 @@ class ThrowableObject extends MoveableObject {
             this.x += 10;
         }, 25);   
         setInterval(() => {
-            this.playAnimation(this.IMAGES_BOTTLE);
+            if(this.isSplash) {
+                this.playAnimation(this.IMAGES_BOTTLE_SPLASH);
+            } else {
+                this.playAnimation(this.IMAGES_BOTTLE);
+            }
         }, 1000 / 60);
+    }
+
+    splash() {
+        this.isSplash = true;
     }
 
 }
